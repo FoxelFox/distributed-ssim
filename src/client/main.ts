9350c7839b387a41384e3bb1384de41eb2ffb63e
André Furchner <storkner@gmail.com>
2019-08-04 13:43:06 +0200

    using image loader

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,26 +1,47 @@
 import { spawn, Thread, Worker, Pool } from "threads"
 import {SSIMWorker} from "./thread";
+import {loadImageBuffer} from "@foxel_fox/glib";
+import * as fs from "fs";
+
+const width = 32;
+const height = 32;
+const channels = 4;
+const bytesPerImage = width * height * channels;
 
 async function run() {
-	const res = await fetch("4.json");
-	const images = await res.json();
-	let csv = "";
+	console.log(location + "/32.jpg");
+	const data = await loadImageBuffer(location + "32.jpg", "OffscreenCanvas"in window);
+	const identifiersRes = await fetch("identifiers.json");
+	const apps = await identifiersRes.json();
 
-	for (const id in images) {
-		images[id].data = new Buffer(images[id].data)
+	const images = {};
+
+	let i = 0;
+	for (const id of apps) {
+		const startIndex = i * bytesPerImage;
+		images[id] = {
+			data: data.slice(startIndex, startIndex + bytesPerImage),
+			width,
+			height,
+			channels
+		};
+		i++
 	}
 
-	let apps = Object.keys(images);
-	const count = apps.length;
+	let csv = "";
 
+	const count = apps.length;
 	const pool = Pool(() => spawn<SSIMWorker>(new Worker("./thread")));
+
+	let results = [];
 
 	try {
 		let index = 0;
 		while (index < apps.length) {
 			const items = apps.slice(index, index + 1);
 			pool.queue(async thread => {
-				csv += await thread.work(items, apps, images);
+				const result = await thread.work(items, apps, images);
+				csv += result;
 				document.body.innerText = csv
 			});
 			index += 1;
